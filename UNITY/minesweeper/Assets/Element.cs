941ae0299a3a3903928fa364032f266935f118ba
Ajay Saini <ajay_saini1006@yahoo.co.in>
2017-03-17 23:31:33 -0700

    UNITY minesweeper game

---
+++ UNITY/minesweeper/Assets/Element.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour {
+    // Is this mine
+    public bool isMine;
+
+    // Different Textures
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+	// Use this for initialization
+	void Start () {
+        // Rendomlt decide if it's a mine or not
+        isMine = Random.value < 0.15;
+
+        // Register in Grid
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+
+        /*// Testing
+        isMine = false;
+        loadTexture(1);*/
+	}
+	
+    // Load another Texture
+    public void loadTexture(int adjacentCount)
+    {
+        if (isMine)
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        else
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+    }
+
+    public bool isCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    void OnMouseUpAsButton()
+    {
+        // It's a mine
+        if (isMine)
+        {
+            // Todo : Uncover all mines
+            Grid.uncoverMines();
+
+            // game over
+            print("YOU LOSE!!");
+        }
+        // not a mine
+        else
+        {
+            // Todo : show adjacent mine number
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            loadTexture(Grid.adjacentMines(x, y));
+
+            // Todo : uncover area without mines
+            Grid.floodFill(x, y, new bool[Grid.w, Grid.h]);
+
+            // Todo : find out if the game was won 
+            if (Grid.isFinished())
+                print("You win");
+        }
+    }
+	/* // Update is called once per frame
+	void Update () {
+		
+	} */
+}
---
+++ UNITY/minesweeper/Assets/Grid.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid {
+    // The Grid
+    public static int w = 10;
+    public static int h = 13;
+    public static Element[,] elements = new Element[w, h];
+
+    // uncover all mines if the user clicked a mine
+    public static void uncoverMines()
+    {
+        foreach (Element elem in elements)
+            if (elem.isMine)
+                elem.loadTexture(0);
+    }
+
+    // find whether an element is a mine
+    public static bool mineAt(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < w && y < h)
+            return elements[x, y].isMine;
+        return false;
+    }
+
+    // find count of adjacent mines
+    public static int adjacentMines(int x, int y)
+    {
+        int count = 0;
+
+        if (mineAt(x, y + 1)) // top
+            count++;
+        if (mineAt(x - 1, y + 1)) // top left
+            count++;
+        if (mineAt(x + 1, y + 1)) // top right
+            count++;
+        if (mineAt(x - 1, y)) // left
+            count++;
+        if (mineAt(x + 1, y)) // right
+            count++;
+        if (mineAt(x, y - 1)) // bottom
+            count++;
+        if (mineAt(x - 1, y - 1)) // bottom left
+            count++;
+        if (mineAt(x + 1, y - 1)) // bottom right
+            count++;
+
+        return count;
+    }
+
+    // flood fill algo if user clicked something other than a mine
+    public static void floodFill(int x, int y, bool[,] visited)
+    {
+        // in range
+        if(x >= 0 && y >= 0 && x < w && y < h)
+        {
+            if (visited[x, y])
+                return;
+
+            // uncover element
+            elements[x, y].loadTexture(adjacentMines(x, y));
+
+            // if not empty cell, then return
+            if (adjacentMines(x, y) > 0)
+                return;
+
+            visited[x, y] = true;
+
+            // recursion
+            floodFill(x, y + 1, visited); //top
+            floodFill(x - 1, y + 1, visited); // top left
+            floodFill(x + 1, y + 1, visited); // top right
+            floodFill(x - 1, y, visited); // left
+            floodFill(x + 1, y, visited); // right
+            floodFill(x, y - 1, visited); // bottom
+            floodFill(x - 1, y - 1, visited); // bottom left
+            floodFill(x + 1, y - 1, visited); // bottom right
+        }
+    }
+
+    // check if all elements other than mine are uncovered
+    public static bool isFinished()
+    {
+        foreach (Element elem in elements)
+            if (elem.isCovered() && !elem.isMine)
+                return false;
+
+        // else all uncovered elements are mine
+        return true;
+    }
+}
